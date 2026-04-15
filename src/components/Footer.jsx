9d458abbb3bf802dfd473423046e53caf8a90633
aiminnano <gabrielxn24@gmail.com>
2026-04-15 15:45:25 +0800

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-
-function Footer(){
-    return  (<footer className="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-    Coded by <a href="#">Aimin</a>.
-  </footer>)
-}
-
-export default Footer;
